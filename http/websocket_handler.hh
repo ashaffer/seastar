0bee0ab69ca565834a305e1580e1ffc1cbc91654
HippoBaro <hippolyte.barraud@epitech.eu>
2017-04-03 02:53:48 +0200

    pretty much done !

--- http/websocket_handler.hh
+++ http/websocket_handler.hh
@@ -26,15 +26,15 @@ protected:
     future_ws_handler_function _f_handle;
 };
 
-typedef std::function<future<>(std::unique_ptr<request>* req, connected_websocket* ws)> future_ws_on_dis_connected;
-typedef std::function<future<>(std::unique_ptr<request>* req, connected_websocket* ws, temporary_buffer<char> message)> future_ws_on_message;
+typedef std::function<future<>(std::unique_ptr<request>* req, websocket_output_stream* ws)> future_ws_on_dis_connected;
+typedef std::function<future<>(std::unique_ptr<request>* req, websocket_output_stream* ws, temporary_buffer<char> message)> future_ws_on_message;
 
 class websocket_handler : public httpd::handler_websocket_base {
 
 public:
-    websocket_handler() : _on_connection([] (std::unique_ptr<request>*, connected_websocket* ws) { return make_ready_future(); }),
-                          _on_message([] (std::unique_ptr<request>*, connected_websocket* ws, temporary_buffer<char> message) { return make_ready_future(); }),
-                          _on_disconnection([] (std::unique_ptr<request>*, connected_websocket* ws) { return make_ready_future(); })  {}
+    websocket_handler() : _on_connection([] (std::unique_ptr<request>*, websocket_output_stream* ws) { return make_ready_future(); }),
+                          _on_message([] (std::unique_ptr<request>*, websocket_output_stream* ws, temporary_buffer<char> message) { return make_ready_future(); }),
+                          _on_disconnection([] (std::unique_ptr<request>*, websocket_output_stream* ws) { return make_ready_future(); })  {}
 
     future<> handle(const sstring &path, connected_websocket ws) override {
         auto input = ws.input();
@@ -42,18 +42,18 @@ public:
         return do_with(std::move(ws), std::move(input), std::move(output), [this] (connected_websocket &ws,
                                                                                    websocket_input_stream &input,
                                                                                    websocket_output_stream &output) {
-            return _on_connection(&ws._request, &ws).then([this, &ws, &input, &output] {
+            return _on_connection(&ws._request, &output).then([this, &ws, &input, &output] {
                 return repeat([this, &input, &output, &ws] {
-                    return input.read().then([this, &ws](temporary_buffer<char> buf){
+                    return input.read().then([this, &output, &ws](temporary_buffer<char> buf){
                         if (!buf)
                             return make_ready_future<bool_class<stop_iteration_tag>>(stop_iteration::yes);
-                        return _on_message(&ws._request, &ws, std::move(buf)).then([] {
+                        return _on_message(&ws._request, &output, std::move(buf)).then([] {
                             return stop_iteration::no;
                         });
                     });
                 });
             });
-        }).then([this, &ws] { return _on_disconnection(&ws._request, &ws); });
+        }).then([this, &ws, &output] { return _on_disconnection(&ws._request, &output); });
     }
 
     void on_message(const future_ws_on_message & handler) { _on_message = future_ws_on_message(handler); }

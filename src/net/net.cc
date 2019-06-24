cd9a196fd5400c48045c66f41a2af73682701950
Andrew Shaffer <darawk@gmail.com>
2019-06-24 00:42:42 -0700

    wip

--- src/net/net.cc
+++ src/net/net.cc
@@ -318,12 +318,15 @@ future<> interface::dispatch_packet(packet p) {
         auto i = _proto_map.find(ntoh(eh->eth_proto));
         if (i != _proto_map.end()) {
             l3_rx_stream& l3 = i->second;
-            forward_hash data;
-            l3.forward(data, p, sizeof(eth_hdr));
-            auto hash = toeplitz_hash(rss_key(), data);
 
-            auto fw = _dev->forward_dst(_dev->hash2qid(hash), [&p, &l3, this, hash] () {
-                return hash;
+            // forward_hash data;            
+            // l3.forward(data, p, sizeof(eth_hdr));
+            // auto hash = toeplitz_hash(rss_key(), data);
+            // auto fw = _dev->forward_dst(_dev->hash2qid(hash), [&p, &l3, this, hash] () {
+            //     return hash;
+            // });
+
+            auto fw = _dev->forward_dst(engine().cpu_id(), [&p, &l3, this, hash] () {
                 
                 auto hwrss = p.rss_hash();
                 if (hwrss) {

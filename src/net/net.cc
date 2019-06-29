61e5e4bb2da112d19f1a6703107756a64ba61274
Andrew Shaffer <darawk@gmail.com>
2019-06-28 22:22:13 -0700

    wip

--- src/net/net.cc
+++ src/net/net.cc
@@ -328,7 +328,7 @@ future<> interface::dispatch_packet(packet p) {
             // uint16_t crc16hash = crc16((char *)data.data, (uint16_t)data.size());
             // printf("\tcrc16hash: %u (0x%x)\n", (uint)_dev->forward_dst(_dev->hash2qid(crc16hash), [crc16hash] () { return (uint)crc16hash; }), (uint)crc16hash);
 
-            uint32_t rss_hash = p.rss_hash();
+            uint32_t rss_hash = p.rss_hash().value();
             printf("Bruteforcing rss_hash: 0x%x\n", rss_hash);
             brute_crc16((const char *)data.data, (size_t)data.size(), (uint16_t)(rss_hash & 0xFFFF));
             brute_crc32(0xFFFFFFFF, (const char *)data.data, (size_t)data.size(), (uint32_t)rss_hash);

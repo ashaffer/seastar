e424ad090f85fac502fa8af1e98f5d7c67a53442
Andrew Shaffer <darawk@gmail.com>
2019-07-02 10:31:44 -0700

    wip

--- src/net/net.cc
+++ src/net/net.cc
@@ -334,11 +334,11 @@ future<> interface::dispatch_packet(packet p) {
 
             data2.push_back(src.ip);
             data2.push_back(dst.ip);
-            data2.push_back((uint8_t)0x0A);
             data2.push_back((uint8_t)0xEA);
+            data2.push_back((uint8_t)0x0A);
 
-            data2.push_back((uint8_t)0x06);
             data2.push_back((uint8_t)0xE6);
+            data2.push_back((uint8_t)0x06);
 
             auto hash2 = toeplitz_hash(rss_key(), data2);
             printf("test hash: 0x%x\n", (uint32_t)hash2);

24ed08794e2544e00ab872d67f0dc2b898374b63
Andrew Shaffer <darawk@gmail.com>
2019-07-07 14:42:31 -0700

    wip

--- src/net/net.cc
+++ src/net/net.cc
@@ -343,11 +343,15 @@ future<> interface::dispatch_packet(packet p) {
             l3_rx_stream& l3 = i->second;
             auto iph = p.get_header<ip_hdr>(sizeof(eth_hdr));
             in_addr in1;
-            in1.s_addr = htonl(iph->src_ip.ip);
+            in1.s_addr = iph->src_ip.ip;
             in_addr in2;
-            in2.s_addr = htonl(iph->dst_ip.ip);
+            in2.s_addr = iph->dst_ip.ip;
 
             printf("interface::dispatch_packet: %s %s\n", inet_ntoa(in1), inet_ntoa(in2));
+            if (iph->src_ip.ip == iph->dst_ip.ip) {
+                printf("src and dst ips equal!?!?!?\n");
+            }
+            
             auto fw = _dev->forward_dst(engine().cpu_id(), [&p, &l3, this] () {
                 auto hwrss = p.rss_hash();
                 if (hwrss) {

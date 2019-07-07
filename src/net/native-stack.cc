df2e218091db7e7937b46dbaf09dae4615de5ce8
Andrew Shaffer <darawk@gmail.com>
2019-07-07 02:56:52 -0700

    wip

--- src/net/native-stack.cc
+++ src/net/native-stack.cc
@@ -214,7 +214,7 @@ native_network_stack::native_network_stack(boost::program_options::variables_map
 
     // for (auto&& device_config : dev_cfgs) {
     printf("bbbb\n");
-    auto&& device_config = dev_cfgs[0];
+    auto& device_config = dev_cfgs[0];
     printf("bbb\n");
         auto& ip_config = device_config.ip_cfg;
     printf("bb\n");

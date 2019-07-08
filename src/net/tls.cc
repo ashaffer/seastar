8a0225b0e26e07191815e8ab6826680abcc5fa6c
Andrew Shaffer <darawk@gmail.com>
2019-07-07 17:19:17 -0700

    wip

--- src/net/tls.cc
+++ src/net/tls.cc
@@ -606,7 +606,7 @@ public:
             connected_socket sock, sstring name = { })
             : session(t, std::move(creds), net::get_impl::get(std::move(sock)),
                     std::move(name)) {
-        printf("Constructing session\n");
+        printf("Constructing session: 0x%x\n", (uint64_t)this);
     }
 
     ~session() {}

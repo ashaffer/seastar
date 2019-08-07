e0fa01b408fe5671b6ed3b296398d7be6e9b7295
Andrew Shaffer <darawk@gmail.com>
2019-08-07 15:56:08 -0700

    wip

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -5762,8 +5762,6 @@ void report_failed_future(const std::exception_ptr& eptr) noexcept {
             std::rethrow_exception(eptr);
         } catch (char const* msg) {
             printf("Caught char const: %s\n", msg);
-        } catch (const char *msg) {
-            printf("Caught const char: %s\n", msg);
         } catch (const std::exception& e) {
             printf("Exceptional future: %s\n", e.what());
         }

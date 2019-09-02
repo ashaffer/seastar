260e114384482b8b06deafc362232e1a7a28d66f
Andrew Shaffer <darawk@gmail.com>
2019-09-02 02:40:59 -0700

    wip

--- include/seastar/http/websocket.hh
+++ include/seastar/http/websocket.hh
@@ -262,7 +262,7 @@ public:
 
     void shutdown_input() { _socket.shutdown_input(); }
 
-    std::chrono::high_resolution_clock::time_point getReceivedAt () const {
+    std::chrono::high_resolution_clock::time_point getReceivedAt () {
         return _socket.getReceivedAt();
     }
 };

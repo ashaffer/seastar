e3e35c9359d9541f7c92b73db6e2928b4136d101
Andrew Shaffer <darawk@gmail.com>
2019-10-13 11:57:08 -0700

    wip

--- include/seastar/core/iostream-impl.hh
+++ include/seastar/core/iostream-impl.hh
@@ -453,7 +453,7 @@ output_stream<CharType>::poll_flush() {
         _buf.trim(_end);
         _end = 0;
         f = _fd.put(std::move(_buf));
-    } else if(_zc_bufs != net::packet::make_null_packet()) {
+    } else if(_zc_bufs != seastar::net::packet::make_null_packet()) {
         f = _fd.put(std::move(_zc_bufs));
     }
 

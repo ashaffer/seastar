b54b4b8aae05c8d154fce760b7a525da67161eb8
Andrew Shaffer <darawk@gmail.com>
2019-11-13 10:49:14 -0800

    wip

--- src/net/net.cc
+++ src/net/net.cc
@@ -339,10 +339,13 @@ uint16_t rte_softrss16(uint16_t *input_tuple, uint32_t input_len,
 
 future<> interface::dispatch_packet(packet p) {
     auto eh = p.get_header<eth_hdr>();
+    printf("dispatch_packet\n");
 
      if (eh) {
+        printf("eh\n");
         auto i = _proto_map.find(ntoh(eh->eth_proto));
         if (i != _proto_map.end()) {
+            printf("found proto\n");
             l3_rx_stream& l3 = i->second;
 
             auto fw = _dev->forward_dst(engine().cpu_id(), [&p, &l3, this] () {
@@ -365,6 +368,7 @@ future<> interface::dispatch_packet(packet p) {
                 printf("Hit incorrect CPU: %u -> %u (%u)\n", engine().cpu_id(), fw, _dev->port_idx());
                 forward(fw, std::move(p));
             } else {
+                printf("no forward necessary: %u\n", (uint)engine().cpu_id());
                 auto h = ntoh(*eh);
                 auto from = h.src_mac;
                 p.trim_front(sizeof(*eh));

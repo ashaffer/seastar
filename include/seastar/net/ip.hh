7627def6714ec18a5e4db71e9331f28817aea3bc
Andrew Shaffer <darawk@gmail.com>
2019-11-13 22:51:29 -0800

    wip

--- include/seastar/net/ip.hh
+++ include/seastar/net/ip.hh
@@ -229,19 +229,19 @@ struct l4connid {
     uint32_t hash(rss_key_type rss_key) {
         forward_hash hash_data;
         if (htonl(local_ip.ip) < htonl(foreign_ip.ip)) {
-            hash_data.push_back(htonl(local_ip.ip));
-            hash_data.push_back(htonl(foreign_ip.ip));
-        } else {
             hash_data.push_back(htonl(foreign_ip.ip));
             hash_data.push_back(htonl(local_ip.ip));           
+        } else {
+            hash_data.push_back(htonl(local_ip.ip));
+            hash_data.push_back(htonl(foreign_ip.ip));
         }
 
         if (htons(local_port) < htons(foreign_port)) {
-            hash_data.push_back(htons(local_port));
             hash_data.push_back(htons(foreign_port));
+            hash_data.push_back(htons(local_port));
         } else {
-            hash_data.push_back(htons(foreign_port));
             hash_data.push_back(htons(local_port));
+            hash_data.push_back(htons(foreign_port));
         }
         return toeplitz_hash(rss_key, hash_data);
     }

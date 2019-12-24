780eae64f2908376bd5ca71debc3cdfbe4dc3cf1
Andrew Shaffer <darawk@gmail.com>
2019-12-24 00:19:42 -0800

    wip

--- include/seastar/net/toeplitz.hh
+++ include/seastar/net/toeplitz.hh
@@ -101,5 +101,28 @@ toeplitz_hash(rss_key_type key, const T& data)
 	return (((hash) & 0xFFFF) << 16) | (hash & 0xFFFF);
 }
 
+template<typename T>
+static inline uint32_t
+toeplitz_hash_full(rss_key_type key, const T& data)
+{
+	uint32_t hash = 0xFFFFFFFF, v;
+	u_int i, b;
+
+	/* XXXRW: Perhaps an assertion about key length vs. data length? */
+
+	v = (key[0]<<24) + (key[1]<<16) + (key[2] <<8) + key[3];
+	for (i = 0; i < data.size(); i++) {
+		for (b = 0; b < 8; b++) {
+			if (data[i] & (1<<(7-b)))
+				hash ^= v;
+			v <<= 1;
+			if ((i + 4) < key.size() &&
+			    (key[i+4] & (1<<(7-b))))
+				v |= 1;
+		}
+	}
+	return hash;
+}
+
 
 }

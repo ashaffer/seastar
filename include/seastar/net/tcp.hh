0f78364486a7a91b3a4a87e8bc3765d698c5d5e1
Andrew Shaffer <darawk@gmail.com>
2019-12-24 00:57:15 -0800

    wip

--- include/seastar/net/tcp.hh
+++ include/seastar/net/tcp.hh
@@ -861,7 +861,7 @@ auto tcp<InetTraits>::connect(socket_address sa, socket_address local) -> connec
              (_inet._inet.netif()->hash2cpu(id.hash(_inet._inet.netif()->rss_key())) != engine().cpu_id()
               || _tcbs.find(id) != _tcbs.end()));
 
-    // printf("sending from %u (0x%x)\n", (uint)engine().cpu_id(), id.hash(_inet._inet.netif()->rss_key()));
+    printf("sending from %u (0x%x)\n", (uint)engine().cpu_id(), id.hash(_inet._inet.netif()->rss_key()));
     // printConnid(id, _inet);
     auto tcbp = make_lw_shared<tcb>(*this, id);
     _tcbs.insert({id, tcbp});

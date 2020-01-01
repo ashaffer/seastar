05afeb4a0143b8d8b941cca6fe15b5640a803f2f
Andrew Shaffer <darawk@gmail.com>
2020-01-01 01:44:47 -0800

    wip

--- src/net/tls.cc
+++ src/net/tls.cc
@@ -590,6 +590,7 @@ public:
             gtls_chk(gnutls_priority_set(*this, prio));
         }
 
+        onTransmitFn = []{};
         gnutls_server_name_set(*this, GNUTLS_NAME_DNS, _hostname.data(), _hostname.size());
         gnutls_transport_set_ptr(*this, this);
         gnutls_transport_set_vec_push_function(*this, &vec_push_wrapper);

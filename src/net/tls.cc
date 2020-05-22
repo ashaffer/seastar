6c46918122e64176e8cc05bdc808a66d49d1f40d
Andrew Shaffer <darawk@gmail.com>
2020-05-22 12:10:33 -0700

    wip

--- src/net/tls.cc
+++ src/net/tls.cc
@@ -80,7 +80,6 @@ static future<temporary_buffer<char>> read_fully(const sstring& name, const sstr
             return f.size().then([&f](uint64_t size) {
                 return f.dma_read_bulk<char>(0, size);
             }).finally([&f]() {
-                printf("read_fully close\n");
                 return f.close();
             });
         });
@@ -971,7 +970,6 @@ public:
                         std::bind(&session::wait_for_eof, this));
     }
     void close() {
-        printf("tls close\n");
         // only do once.
         if (!std::exchange(_shutdown, true)) {
             auto me = shared_from_this();
@@ -979,12 +977,10 @@ public:
             (void)with_timeout(timer<>::clock::now() + std::chrono::seconds(10), shutdown()).finally([this] {
                 _eof = true;
                 try {
-                    printf("tls in close\n");
                     (void)_in.close().handle_exception([](std::exception_ptr) {}); // should wake any waiters
                 } catch (...) {
                 }
                 try {
-                    printf("tls out close\n");
                     (void)_out.close().handle_exception([](std::exception_ptr) {});
                 } catch (...) {
                 }
@@ -1069,11 +1065,9 @@ public:
     data_sink sink() override;
 
     void shutdown_input() override {
-        printf("shutdown_input close\n");
         _session->close();
     }
     void shutdown_output() override {
-        printf("shutdown_output close\n");
         _session->close();
     }
     void set_nodelay(bool nodelay) override {
@@ -1117,7 +1111,6 @@ private:
         return _session->get();
     }
     future<> close() override {
-        printf("source_impl session close\n");
         _session->close();
         return make_ready_future<>();
     }
@@ -1139,7 +1132,6 @@ private:
         return _session->put(std::move(p));
     }
     future<> close() override {
-        printf("sink_impl session close\n");
         _session->close();
         return make_ready_future<>();
     }

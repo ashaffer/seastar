b8d1e70e2b5dfd37531e27180060b202d663431f
Andrew Shaffer <darawk@gmail.com>
2021-01-12 13:31:07 -0800

    wip

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -3044,15 +3044,17 @@ bool smp_message_queue::pure_poll_tx() const {
 }
 
 void smp_message_queue::submit_item(shard_id t, std::unique_ptr<smp_message_queue::work_item> item, bool ignoreLimits) {
-    // if (ignoreLimits) {
-    //     _tx.a.pending_fifo.push_back(item.get());
-    //     // no exceptions from this point
-    //     item.release();
-    //     // u.release();
-    //     if (_tx.a.pending_fifo.size() >= batch_size) {
-    //         move_pending();
-    //     }        
-    // } else {
+    if (ignoreLimits) {
+        printf("ignoreLimits\n");
+        _tx.a.pending_fifo.push_back(item.get());
+        // no exceptions from this point
+        item.release();
+        // u.release();
+        if (_tx.a.pending_fifo.size() >= batch_size) {
+            move_pending();
+        }        
+    } else {
+        printf("useLimits\n");
         // matching signal() in process_completions()
         auto ssg_id = internal::smp_service_group_id(item->ssg);
         auto& sem = smp_service_groups[ssg_id].clients[t];
@@ -3066,7 +3068,7 @@ void smp_message_queue::submit_item(shard_id t, std::unique_ptr<smp_message_queu
                 move_pending();
             }
         });
-    // }
+    }
 }
 
 void smp_message_queue::respond(work_item* item) {

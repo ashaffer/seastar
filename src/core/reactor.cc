e64cbccb1fc28bae66b5bd412872cf1ba5204155
Andrew Shaffer <darawk@gmail.com>
2021-01-12 13:50:28 -0800

    wip

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -3051,9 +3051,9 @@ void smp_message_queue::submit_item(shard_id t, std::unique_ptr<smp_message_queu
             // no exceptions from this point
             item.release();
             // u.release();
-            if (_tx.a.pending_fifo.size() >= batch_size) {
-                move_pending();
-            }
+            // if (_tx.a.pending_fifo.size() >= batch_size) {
+            //     move_pending();
+            // }
             printf("end ignoreLimits\n");
         });
     } else {

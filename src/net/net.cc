c8e0d02ad56c521f111057f73d6b842c548c7330
Andrew Shaffer <darawk@gmail.com>
2021-02-10 16:11:54 -0800

    wip

--- src/net/net.cc
+++ src/net/net.cc
@@ -85,6 +85,7 @@ bool qp::poll_tx() {
                 auto p = pr();
                 if (p) {
                     work++;
+                    p.value().notifyTransmitted(std::chrono::high_resolution_clock::now());
                     _tx_packetq.push_back(std::move(p.value()));
                     if (_tx_packetq.size() == 128) {
                         break;

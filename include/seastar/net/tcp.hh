e0e98f50f561c6e1740ffaae9517d3c7177b7319
Andrew <darawk@gmail.com>
2021-05-18 22:08:36 -0700

    wip

--- include/seastar/net/tcp.hh
+++ include/seastar/net/tcp.hh
@@ -983,6 +983,8 @@ void printConnid (Connid &connid, Inet &inet) {
 
 template <typename InetTraits>
 void tcp<InetTraits>::received(packet p, ipaddr from, ipaddr to) {
+    printf("received: %u\n", p.len());
+    
     auto th = p.get_header(0, tcp_hdr::len);
     if (!th) {
         return;

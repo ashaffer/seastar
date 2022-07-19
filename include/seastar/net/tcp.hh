68e4bce2eea0db0a580e98917e81ffb767948edc
Andrew <darawk@gmail.com>
2022-07-19 00:27:11 -0700

    wip

--- include/seastar/net/tcp.hh
+++ include/seastar/net/tcp.hh
@@ -1002,6 +1002,7 @@ void tcp<InetTraits>::received(packet p, ipaddr from, ipaddr to) {
     }
     auto h = tcp_hdr::read(th);
     auto id = connid{to, from, h.dst_port, h.src_port};
+    printConnid(id, _inet);
     auto tcbi = _tcbs.find(id);
 
     lw_shared_ptr<tcb> tcbp;

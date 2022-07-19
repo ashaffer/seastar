10b2af4f9466e885d270e5608f3927092d2e1459
Andrew <darawk@gmail.com>
2022-07-19 14:30:00 -0700

    wip

--- src/net/net.cc
+++ src/net/net.cc
@@ -373,6 +373,7 @@ future<> interface::dispatch_packet(packet p) {
                 }
             });
 
+            printf("test\n");
             if (fw != engine().cpu_id()) {
                 printf("Hit incorrect CPU: %u -> %u (%u)\n", engine().cpu_id(), fw, _dev->port_idx());
                 forward(fw, std::move(p));

304f186e1c8d3310c87a3c2a52a535620d2edce1
Andrew <darawk@gmail.com>
2022-07-23 00:07:32 -0700

    wip

--- include/seastar/core/future-util.hh
+++ include/seastar/core/future-util.hh
@@ -1197,7 +1197,6 @@ future<T...> with_timeout(std::chrono::time_point<Clock, Duration> timeout, futu
     (void)f.then_wrapped([pr = std::move(pr), timer = std::move(timer)] (auto&& f) mutable {
         if (timer.cancel()) {
             f.forward_to(std::move(*pr));
-            f.ignore_ready_future();
         } else {
             f.ignore_ready_future();
         }

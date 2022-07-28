8f2aa71034db5d354a755f94e99a8f27a9e2b04a
Andrew <darawk@gmail.com>
2022-07-27 21:55:56 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1925,8 +1925,10 @@ bool dpdk_qp<HugetlbfsMemBackend>::map_dma()
 {
     auto m = memory::get_memory_layout();
     rte_iova_t iova = rte_mem_virt2iova((const void*)m.start);
-
-    return rte_vfio_dma_map(m.start, iova, m.end - m.start) == 0;
+    printf("pre dma_map: %lu\n", (unsigned long)(m.end - m.start));
+    bool res = rte_vfio_dma_map(m.start, iova, m.end - m.start) == 0;
+    printf("post dma map\n");
+    return res;
 }
 
 void dpdk_device::check_port_link_status()

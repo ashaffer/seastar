7bc5029e8e6ba436fb91b693d6426c7cc43216b5
Andrew <darawk@gmail.com>
2022-07-28 16:00:26 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -2282,6 +2282,7 @@ bool dpdk_qp<HugetlbfsMemBackend>::poll_rx_once()
 
     /* Now process the NIC packets read */
     if (likely(rx_count > 0)) {
+        printf("process_packets: %u\n", (uint)rx_count);
         process_packets(
             buf, 
             rx_count, 

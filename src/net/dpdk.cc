2b9311938419ff80392624b437e7c0d58a9bca1d
Andrew <darawk@gmail.com>
2022-07-29 12:59:37 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1246,17 +1246,19 @@ public:
     virtual ~dpdk_qp() { }
 
     virtual uint32_t send(circular_buffer<packet>& pb) override {
-        // if (HugetlbfsMemBackend) {
-        //     // Zero-copy send
-        //     return _send(pb, [&] (packet&& p) {
-        //         return tx_buf::from_packet_zc(std::move(p), *this);
-        //     });
-        // } else {
+        return pb.size();
+
+        if (HugetlbfsMemBackend) {
+            // Zero-copy send
+            return _send(pb, [&] (packet&& p) {
+                return tx_buf::from_packet_zc(std::move(p), *this);
+            });
+        } else {
             // "Copy"-send
             return _send(pb, [&](packet&& p) {
                 return tx_buf::from_packet_copy(std::move(p), *this);
             });
-        // }
+        }
     }
 
     dpdk_device& port() const { return *_dev; }

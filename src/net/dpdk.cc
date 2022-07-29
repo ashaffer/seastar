60b73f87a196861341339653f50960e99a353544
Andrew <darawk@gmail.com>
2022-07-29 14:22:48 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -674,7 +674,7 @@ build_mbuf_cluster:
 
             unsigned total_nsegs = nsegs;
 
-            printf("Sending packet: ");
+            printf("Sending packet (zc): ");
             for (unsigned i = 1; i < p.nr_frags(); i++) {
                 rte_mbuf *h = nullptr, *new_last_seg = nullptr;
                 if (!translate_one_frag(qp, p.frag(i), h, new_last_seg, nsegs)) {
@@ -790,11 +790,11 @@ build_mbuf_cluster:
                 return nullptr;
             }
 
-            printf("Sending packet: ");
+            printf("Sending packet (copy): ");
             for (uint i = 0; i < p.nr_frags(); i++) {
                 struct fragment f = p.frag(i);
                 for (uint j = 0; j < f.size; j++) {
-                    printf("%02x ", (uint8_t)f.base[j]);
+                    printf("%02x ", (uint8_t)(f.base[j] & 0xFF));
                 }
             }
             printf("\n");

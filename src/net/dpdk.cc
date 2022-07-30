27f871fd14d2f6b9c6fe21de20be5af8fdaefe45
Andrew <darawk@gmail.com>
2022-07-30 13:20:49 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1904,11 +1904,11 @@ bool dpdk_qp<HugetlbfsMemBackend>::init_rx_mbuf_pool()
         // 2) Bind data buffers to each of them.
         // 3) Return them back to the pool.
 
-        // for (int i = 0; i < mbufs_per_queue_rx; i++) {
-        //     rte_mbuf* m = rte_pktmbuf_alloc(_pktmbuf_pool_rx);
-        //     assert(m);
-        //     _rx_free_bufs.push_back(m);
-        // }
+        for (int i = 0; i < mbufs_per_queue_rx; i++) {
+            rte_mbuf* m = rte_pktmbuf_alloc(_pktmbuf_pool_rx);
+            assert(m);
+            _rx_free_bufs.push_back(m);
+        }
 
         // for (auto&& m : _rx_free_bufs) {
         //     if (!init_noninline_rx_mbuf(m)) {
@@ -1940,8 +1940,8 @@ bool dpdk_qp<HugetlbfsMemBackend>::init_rx_mbuf_pool()
         // mbuf = _rx_free_bufs[mbufs_per_queue_rx - 1];
         // printf("Allocated mbuf: 0x%lx (0x%lx, 0x%lx)\n", (uint64_t)mbuf, (uint64_t)mbuf->buf_addr, mbuf->buf_iova);
 
-        // rte_mempool_put_bulk(_pktmbuf_pool_rx, (void**)_rx_free_bufs.data(),
-        //                      _rx_free_bufs.size());
+        rte_mempool_put_bulk(_pktmbuf_pool_rx, (void**)_rx_free_bufs.data(),
+                             _rx_free_bufs.size());
 
         printf("init_rx_mbuf_pool: 0x%lx, 0x%lx\n", (uint64_t)_rx_free_bufs.data(), (uint64_t)_pktmbuf_pool_rx);
 

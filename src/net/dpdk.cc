010122441653b4321df14ae8b9ca04b863ac3510
Andrew <darawk@gmail.com>
2022-07-29 17:09:44 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1903,19 +1903,19 @@ bool dpdk_qp<HugetlbfsMemBackend>::init_rx_mbuf_pool()
         // 2) Bind data buffers to each of them.
         // 3) Return them back to the pool.
 
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < mbufs_per_queue_rx; i++) {
             rte_mbuf* m = rte_pktmbuf_alloc(_pktmbuf_pool_rx);
             assert(m);
             _rx_free_bufs.push_back(m);
         }
 
-        for (auto&& m : _rx_free_bufs) {
-            if (!init_noninline_rx_mbuf(m)) {
-                printf("Failed to allocate data buffers for Rx ring. "
-                       "Consider increasing the amount of memory.\n");
-                exit(1);
-            }
-        }
+        // for (auto&& m : _rx_free_bufs) {
+        //     if (!init_noninline_rx_mbuf(m)) {
+        //         printf("Failed to allocate data buffers for Rx ring. "
+        //                "Consider increasing the amount of memory.\n");
+        //         exit(1);
+        //     }
+        // }
 
         rte_mempool_put_bulk(_pktmbuf_pool_rx, (void**)_rx_free_bufs.data(),
                              _rx_free_bufs.size());

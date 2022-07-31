530e912f46dac9869557d0f5327aec57bfefb19d
Andrew <darawk@gmail.com>
2022-07-30 19:00:35 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1912,6 +1912,7 @@ bool dpdk_qp<HugetlbfsMemBackend>::init_rx_mbuf_pool()
         printf("here\n");
         for (int i = 0; i < mbufs_per_queue_rx; i++) {
             rte_mbuf* m = rte_pktmbuf_alloc(_pktmbuf_pool_rx);
+            printf("m: 0x%lx\n", (uint64_t)m);
             assert(m);
             _rx_free_bufs.push_back(m);
         }

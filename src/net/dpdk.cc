69a646fc55b6cd3d20f39e19938338bdf49a27c7
Andrew <darawk@gmail.com>
2022-07-31 11:47:34 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -2009,7 +2009,7 @@ bool dpdk_qp<HugetlbfsMemBackend>::init_rx_mbuf_pool()
         // rte_rwlock_write_lock(RTE_EAL_MEMPOOL_RWLOCK);
         char mzname[32] = {0};
         sprintf(mzname, "testing%u", engine().cpu_id());
-        const struct rte_memzone *mz = rte_memzone_reserve(mzname, _rx_free_bufs.size() * mbuf_data_size, rte_socket_id(), RTE_MEMZONE_2MB | RTE_MEMZONE_SIZE_HINT_ONLY);
+        const struct rte_memzone *mz = rte_memzone_reserve(mzname, _rx_free_bufs.size() * mbuf_data_size, rte_socket_id(), RTE_MEMZONE_2MB);
         // rte_rwlock_write_unlock(RTE_EAL_MEMPOOL_RWLOCK);
 
         if (mz == NULL) {

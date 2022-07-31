64baff0eeaa444c4f40ddbfd47d3662926dafef3
Andrew <darawk@gmail.com>
2022-07-30 22:11:24 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1938,7 +1938,7 @@ bool dpdk_qp<HugetlbfsMemBackend>::init_rx_mbuf_pool()
                 exit(1);
             }
 
-            m->buf_iova += 4;
+            m->buf_iova += 100;
         }
 
         // if (engine().cpu_id() == 0) {

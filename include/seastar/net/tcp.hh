5bff00d895960d713db301b479d9e6e29c663467
Andrew <darawk@gmail.com>
2022-08-01 23:46:59 -0700

    wip

--- include/seastar/net/tcp.hh
+++ include/seastar/net/tcp.hh
@@ -515,6 +515,12 @@ private:
             });
         }
         future<> connect_done() {
+            printf("connect_done\n");
+            _connect_done.get_future().then([] () {
+                printf("connection established\n");
+            }).catch([] (std::exception_ptr e) {
+                printf("connection error\n");
+            });
             return _connect_done.get_future();
         }
         tcp_state& state() {

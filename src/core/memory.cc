b0d1018d3fff117e5b208a4f0ad2a1fbff5516d1
Andrew <darawk@gmail.com>
2022-08-05 11:38:51 -0700

    wip

--- src/core/memory.cc
+++ src/core/memory.cc
@@ -914,7 +914,9 @@ allocate_anonymous_memory(compat::optional<void*> where, size_t how_much) {
 mmap_area
 allocate_hugetlbfs_memory(file_desc& fd, compat::optional<void*> where, size_t how_much) {
     auto pos = fd.size();
+    printf("Fd truncate: 0x%x, 0x%x, 0x%x\n", pos, how_much, pos + how_much);
     fd.truncate(pos + how_much);
+    printf("post truncate\n");
 
     auto ret = fd.map(
             how_much,
--- src/net/net.cc
+++ src/net/net.cc
@@ -359,7 +359,6 @@ future<> interface::dispatch_packet(packet p) {
             auto fw = _dev->forward_dst(engine().cpu_id(), [&p, &l3, this] () {
                 auto hwrss = p.rss_hash();
                 if (hwrss) {
-                    printf("HW RSS Hash: 0x%x\n", hwrss.value());
                     return hwrss.value();
                 } else {
                     forward_hash data;

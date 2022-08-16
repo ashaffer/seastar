62bfe7e4827959abbe07092c892f743d8be817af
Andrew <darawk@gmail.com>
2022-08-15 23:24:23 -0700

    wip

--- src/net/ip.cc
+++ src/net/ip.cc
@@ -34,7 +34,6 @@ namespace net {
 ipv4_address::ipv4_address(const std::string& addr) {
     boost::system::error_code ec;
     auto ipv4 = boost::asio::ip::address_v4::from_string(addr, ec);
-    printf("ipv4: 0x%lx\n", ipv4.to_ulong());
     if (ec) {
         throw std::runtime_error(
             format("Wrong format for IPv4 address {}. Please ensure it's in dotted-decimal format", addr));
--- src/net/net.cc
+++ src/net/net.cc
@@ -49,6 +49,7 @@ ipv4_addr::ipv4_addr(const std::string &addr) {
     boost::split(items, addr, boost::is_any_of(":"));
     if (items.size() == 1) {
         ip = boost::asio::ip::address_v4::from_string(addr).to_ulong();
+        printf("ipv4_addr: 0x%x\n", ip);
         port = 0;
     } else if (items.size() == 2) {
         ip = boost::asio::ip::address_v4::from_string(items[0]).to_ulong();

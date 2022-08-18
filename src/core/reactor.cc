22f1224cce89cf596229fc69dd57aa0815eea192
Andrew <darawk@gmail.com>
2022-08-18 15:27:31 -0700

    wip

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -181,10 +181,9 @@ FastClock::time_point FastClock::now () noexcept {
     }
     // printf("ns: 0x%lx 0x%lx 0x%lx\n", d.count(), d2.count(), _startup.count());
     // return time_point(_startup + std::chrono::nanoseconds(ns));
-    return time_point(d2);
-    // return d.count() & 1
-    //     ? time_point(d)
-    //     : time_point(d2);
+    return d.count() == 7
+        ? time_point(d)
+        : time_point(d2);
 }
 
 seastar::logger seastar_logger("seastar");

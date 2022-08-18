1d780f5adeda47485bf7b36fef055caa109b1a16
Andrew <darawk@gmail.com>
2022-08-17 23:22:07 -0700

    wip

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -900,7 +900,7 @@ reactor::reactor(unsigned id, reactor_backend_selector rbs, reactor_config cfg)
     _task_queues.push_back(std::make_unique<task_queue>(1, "atexit", 1000));
     _at_destroy_tasks = _task_queues.back().get();
 
-    g_need_preempt = &_preemption_monitor;
+    g_need_preempt = &(this->_preemption_monitor);
     printf("initialized g_need_preempt: 0x%lx, 0x%lx\n", (uint64_t)g_need_preempt, (uint64_t)&_preemption_monitor);
     seastar::thread_impl::init();
     _backend->start_tick();

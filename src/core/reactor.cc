0ff11bfa30992607c747a2ec2fb9670513bfac06
Andrew <darawk@gmail.com>
2022-08-18 22:34:27 -0700

    wip

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -165,7 +165,7 @@ FastClock::time_point FastClock::now () noexcept {
     static thread_local std::chrono::nanoseconds _startup{};
     static thread_local uint64_t _startup_ticks = 0;
 
-    asm("mfence");
+    // asm("mfence");
     uint64_t ticks = __rdtsc();
 
     if (_startup_ticks == 0) {

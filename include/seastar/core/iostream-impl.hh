926fa38ca29c19c603c3fd8cf57dfa63bf55151b
Andrew <darawk@gmail.com>
2022-08-29 11:26:57 -0700

    wip

--- include/seastar/core/iostream-impl.hh
+++ include/seastar/core/iostream-impl.hh
@@ -296,17 +296,10 @@ input_stream<CharType>::read() {
         return make_ready_future<tmp_buf>();
     }
     if (_buf.empty()) {
-        auto f = _fd.get().then([this] (tmp_buf buf) {
+        return _fd.get().then([this] (tmp_buf buf) {
             _eof = buf.empty();
             return make_ready_future<tmp_buf>(std::move(buf));
         });
-
-        f.handle_exception([] (auto ep) {
-            printf("input_stream::read exception\n");
-            return make_exception_future(ep);
-        }).discard_result();
-
-        return std::move(f);
     } else {
         return make_ready_future<tmp_buf>(std::move(_buf));
     }

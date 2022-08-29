211d8631d079dfac42fcc4fcb5ed82c76a5b6319
Andrew <darawk@gmail.com>
2022-08-28 22:51:22 -0700

    wip

--- include/seastar/net/tcp.hh
+++ include/seastar/net/tcp.hh
@@ -1033,7 +1033,7 @@ void tcp<InetTraits>::received(packet p, ipaddr from, ipaddr to) {
             //      if ACK off: <SEQ=0><ACK=SEG.SEQ+SEG.LEN><CTL=RST,ACK>
             //      if ACK on:  <SEQ=SEG.ACK><CTL=RST>
             printf("respond_with_reset 1: %u, %u\n", listener == _listening.end(), listener->second->full());
-            printConnid(id);
+            printConnid(id, _inet);
             return respond_with_reset(&h, id.local_ip, id.foreign_ip);
         } else {
             // 2) In LISTEN state

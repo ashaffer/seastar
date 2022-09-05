bca2e49e86ef04a590316fa5da52af162f5bcdd2
Andrew <darawk@gmail.com>
2022-09-05 00:06:22 -0700

    wip

--- src/net/tls.cc
+++ src/net/tls.cc
@@ -1240,7 +1240,7 @@ private:
     }
     using data_sink_impl::put;
     future<> put(net::packet p) override {
-        p.notifyTransmitted(__rdtsc(), 4);
+        p.notifyTransmitted(__rdtsc(), 3);
         return _session->put(std::move(p)).handle_exception([] (std::exception_ptr ep) {
             try {
                 std::rethrow_exception(ep);

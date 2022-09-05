e70e182fd4861dc00ac1d02cd3788b075b434878
Andrew <darawk@gmail.com>
2022-09-05 15:17:57 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1068,11 +1068,12 @@ build_mbuf_cluster:
             }
             auto t3 = ticks();
             size_t len = std::min(buf_len, max_frag_len);
+            auto old_iova = buf->buf_iova;
             buf->set_zc_info(va, iova, len);
             m = buf->rte_mbuf_p();
             auto t4 = ticks();
-            later().then([t1, t2, t3, t4, va, iova] () {
-                printf("set_one_data_buf: %uns, %uns, %uns (%u, 0x%lx, 0x%lx)\n", ticks_to_ns(t2 - t1), ticks_to_ns(t3 - t2), ticks_to_ns(t4 - t3), (uint)engine().cpu_id(), (uint64_t)iova, (uint64_t)va);
+            later().then([buf, t1, t2, t3, t4, va, old_iova] () {
+                printf("set_one_data_buf: %uns, %uns, %uns (%u, 0x%lx, 0x%lx, 0x%lx)\n", ticks_to_ns(t2 - t1), ticks_to_ns(t3 - t2), ticks_to_ns(t4 - t3), (uint)engine().cpu_id(), (uint64_t)iova, (uint64_t)va, (uint64_t)old_iova);
             });
             return len;
         }

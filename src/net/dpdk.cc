e3c83157fb74c5b41fd8cf2810c9e94d2ba0740b
Andrew <darawk@gmail.com>
2022-09-05 23:45:20 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1096,7 +1096,7 @@ build_mbuf_cluster:
             m = buf->rte_mbuf_p();
             auto t4 = ticks();
             later().then([t1, t2, t3, t4, va, iova, old_iova, buf_iova = buf->_buf_iova, iova2] () {
-                printf("iovas: 0x%x, 0x%lx\n", iova, iova2);
+                printf("iovas: 0x%lx, 0x%lx\n", iova, iova2);
                 printf("set_one_data_buf: %uns, %uns, %uns (%u, 0x%lx, 0x%lx, 0x%lx, 0x%lx)\n",
                     ticks_to_ns(t2 - t1), ticks_to_ns(t3 - t2),
                     ticks_to_ns(t4 - t3), (uint)engine().cpu_id(),

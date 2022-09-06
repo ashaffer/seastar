61f7cc74dab65b37c86af1c82d19e1edb68794c5
Andrew <darawk@gmail.com>
2022-09-06 01:12:26 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1360,28 +1360,16 @@ public:
     virtual ~dpdk_qp() { }
 
     virtual uint32_t send(circular_buffer<packet>& pb) override {
-        uint64_t t1e = 0;
-        uint64_t t2e = 0;
-
         if (HugetlbfsMemBackend) {
-            auto t1 = ticks();
             // Zero-copy send
-            // auto t1 = ticks();
-            auto result = _send(pb, [&] (packet&& p) {
+            return _send(pb, [&] (packet&& p) {
                 return tx_buf::from_packet_zc(std::move(p), *this);
-            }, t1e, t2e);
-            auto t2 = ticks();
-            later().then([t1, t2, t1e, t2e]() {
-                printf("outer send %u: %uns, %uns, %uns, %uns, %uns\n",
-                    engine().cpu_id(), ticks_to_ns(t1e - t1), ticks_to_ns(t2e - t1e), ticks_to_ns(t2 - t2e),
-                    ticks_to_ns(t2 - t2e), ticks_to_ns(t2 - t1));
             });
-            return result;
         } else {
             // "Copy"-send
             return _send(pb, [&](packet&& p) {
                 return tx_buf::from_packet_copy(std::move(p), *this);
-            }, t1e, t2e);
+            });
         }
     }
 
@@ -1390,8 +1378,7 @@ public:
 private:
 
     template <class Func>
-    uint32_t _send(circular_buffer<packet>& pb, Func packet_to_tx_buf_p, uint64_t& t, uint64_t& t2) {
-        t = ticks();
+    uint32_t _send(circular_buffer<packet>& pb, Func packet_to_tx_buf_p) {
         if (_tx_burst.size() == 0) {
             uint64_t start = ticks();
 
@@ -1437,11 +1424,6 @@ private:
             });
         }
 
-        t2 = ticks();
-        // later().then([t, t0, t4] () {
-        //     printf("send %u: %uns, %uns\n",
-        //         (uint)engine().cpu_id(), ticks_to_ns(t0 - t), ticks_to_ns(t4 - t0));
-        // });
         // else {
         //     printf("Failed to transmit all packets\n");
         // }

25f89c77e44dbd322c87eba1e3627e3531db62b9
adsf <darawk@gmail.com>
2024-06-22 12:33:36 -0700

    wip

--- src/net/native-stack.cc
+++ src/net/native-stack.cc
@@ -128,13 +128,17 @@ void create_native_net_device(boost::program_options::variables_map opts) {
 
                 if (qid < sdev->hw_queues_count()) {
                     auto qp = sdev->init_local_queue(opts, qid);
+                    printf("Local queues initialized\n");
                     std::map<unsigned, float> cpu_weights;
                     for (unsigned i = sdev->hw_queues_count() + qid % sdev->hw_queues_count(); i < smp::count; i+= sdev->hw_queues_count()) {
                         cpu_weights[i] = 1;
                     }
                     cpu_weights[qid] = opts.find("hw-queue-weight")->second.as<float>();
+                    printf("Configutring proxies...\n");
                     qp->configure_proxies(cpu_weights);
+                    printf("Proxies configured, setting local queue...\n");
                     sdev->set_local_queue(std::move(qp), qid);
+                    printf("Local queue set\n");
                 } else {
                     auto master_qid = qid % sdev->hw_queues_count();
                     auto master_cpuid = sdev->qid2cpuid(master_qid);

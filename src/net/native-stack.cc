a238070aaa56fd29e2003c929ff30ec7bc797125
adsf <darawk@gmail.com>
2024-06-24 15:15:09 -0700

    wip

--- src/net/native-stack.cc
+++ src/net/native-stack.cc
@@ -133,7 +133,8 @@ void create_native_net_device(const boost::program_options::variables_map& opts)
                 if (qid < sdev->hw_queues_count()) {
                     auto qp = sdev->init_local_queue(opts, qid);
                     printf("here\n");
-                    printf("there: %u\n", smp::count);
+                    printf("there: %u, %u\n", smp::count, qid);
+                    printf("test: %u\n", sdev->hw_queues_count());
                     std::map<unsigned, float> cpu_weights;
                     for (unsigned i = sdev->hw_queues_count() + qid % sdev->hw_queues_count(); i < smp::count; i+= sdev->hw_queues_count()) {
                         cpu_weights[i] = 1;

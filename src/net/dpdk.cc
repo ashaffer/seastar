8c1c7a87c56c99c44ca814ef73435dd921e0f399
adsf <darawk@gmail.com>
2024-06-28 14:43:57 -0700

    wip

--- src/net/dpdk.cc
+++ src/net/dpdk.cc
@@ -1390,7 +1390,6 @@ public:
     }
 
     virtual uint32_t send(circular_buffer<packet>& pb) override {
-        printf("cb packet: %lu\n", pb.size());
         if (HugetlbfsMemBackend) {
             // Zero-copy send
             return _send(pb, [&] (packet&& p) {

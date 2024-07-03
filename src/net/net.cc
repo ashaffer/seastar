7542699820523e60ce200d1bb409443d8bdd6e02
adsf <darawk@gmail.com>
2024-07-03 16:42:51 -0700

    wip

--- src/net/net.cc
+++ src/net/net.cc
@@ -197,16 +197,14 @@ qp::qp(bool register_copy_stats,
     }
 }
 
-qp::~qp() {
-    printf("regular qp destroyed\n");
-}
+qp::~qp() {}
 
 void qp::configure_proxies(const std::map<unsigned, float>& cpu_weights) {
     assert(!cpu_weights.empty());
     // if ((cpu_weights.size() == 1 && cpu_weights.begin()->first == engine().cpu_id())) {
     //     // special case queue sending to self only, to avoid requiring a hash value
     //     return;
-    // }
+    // }F
     register_packet_provider([this] {
         std::optional<packet> p;
         if (!_proxy_packetq.empty()) {

6d8b12ae2bb7bb7f7b477fffe0830c70b56fae90
adsf <darawk@gmail.com>
2025-06-04 20:13:59 -0700

    wip

--- include/seastar/net/tcp.hh
+++ include/seastar/net/tcp.hh
@@ -972,7 +972,8 @@ auto tcp<InetTraits>::connect(socket_address sa, socket_address local) -> connec
              (netif->hash2cpu(id.hash(rss_conf)) != engine().cpu_id()
               || _tcbs.find(id) != _tcbs.end()));
 
-    // printConnid(id, _inet);
+    printf("outgoing:\n");
+    printConnid(id, _inet);
     auto tcbp = make_lw_shared<tcb>(*this, id);
     _tcbs.insert({id, tcbp});
     printf("\tseastar connecting\n");
